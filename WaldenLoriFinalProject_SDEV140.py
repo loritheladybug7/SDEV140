339bc021253b0af96a8f8e1c5e7661aee86a336a
lori <155836726+loritheladybug7@users.noreply.github.com>
2024-05-10 12:16:52 -0400

    Update WaldenLoriFinalProject_SDEV140.py
    Added more documentation and improved input validation

--- WaldenLoriFinalProject_SDEV140.py
+++ WaldenLoriFinalProject_SDEV140.py
@@ -34,26 +34,27 @@ exp2_label = tk.Label(root, text="a d4 has 4 sides and can roll 1-4.")
 exp2_label.grid(row=3, column=0, columnspan=3)
 
 
-# Function for input validation
+# Function for input validation, checked when spinboxes are changed
 def validate_input(value):
     if value.isdigit():
         if 0 <= int(value) <= 50:
             return True
-    messagebox.showerror("Error", "Please enter a valid integer for the number of sides.")
+    messagebox.showerror("Error", "Please enter a valid integer.")
     return
     
 # Create labels and entry fields for input
-  # d4
-d4_label = tk.Label(root, text="Quantity of d4's: ")
-d4_label.grid(row=5, column=0)
+  # Entry for a d4 - die with four sides
+d4_label = tk.Label(root, text="Quantity of d4's: ")  # d4 Label for spinbox/entry
+d4_label.grid(row=5, column=0)                        # Grid location for d4 Label
 d4_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
-                        validatecommand=(root.register(validate_input), "%P"))
-d4_entry.grid(row=5, column=1)
-d4_image = PhotoImage(file="d4_50.gif")
-d4_img = Label(root, image=d4_image)
-d4_img.grid(row=5, column=2)
+                        validatecommand=(root.register(validate_input), "%P")) 
+         # Spinbox for d4 entry that validates the input and pops up an error if invalid
+d4_entry.grid(row=5, column=1)                        # Grid location for d4 entry
+d4_image = PhotoImage(file="d4_50.gif")               # Image for d4    
+d4_img = Label(root, image=d4_image)                  # Label for d4 image
+d4_img.grid(row=5, column=2)                          # Grid location for d4 image
 
-  # d6
+  # Entry for a d6 - die with six sides
 d6_label = tk.Label(root, text="Quantity of d6's: ")
 d6_label.grid(row=6, column=0)
 d6_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
@@ -63,7 +64,7 @@ d6_image = PhotoImage(file="d6_50.gif")
 d6_img = Label(root, image=d6_image)
 d6_img.grid(row=6, column=2)
 
-  # d8
+  # Entry for a d8 - die with eight sides
 d8_label = tk.Label(root, text="Quantity of d8's: ")
 d8_label.grid(row=7, column=0)
 d8_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
@@ -73,7 +74,7 @@ d8_image = PhotoImage(file="d8_50.gif")
 d8_img = Label(root, image=d8_image)
 d8_img.grid(row=7, column=2)
 
-  # d10
+ # Entry for a d10 - die with ten sides
 d10_label = tk.Label(root, text="Quantity of d10's: ")
 d10_label.grid(row=8, column=0)
 d10_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
@@ -83,7 +84,7 @@ d10_image = PhotoImage(file="d10_50.gif")
 d10_img = Label(root, image=d10_image)
 d10_img.grid(row=8, column=2)
 
-  # d12
+ # Entry for a d12 - die with twelve sides
 d12_label = tk.Label(root, text="Quantity of d12's: ")
 d12_label.grid(row=9, column=0)
 d12_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
@@ -93,7 +94,7 @@ d12_image = PhotoImage(file="d12_50.gif")
 d12_img = Label(root, image=d12_image)
 d12_img.grid(row=9, column=2)
 
-  # d20
+ # Entry for a d20 - die with twenty sides
 d20_label = tk.Label(root, text="Quantity of d20's: ")
 d20_label.grid(row=10, column=0)
 d20_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
@@ -103,14 +104,15 @@ d20_image = PhotoImage(file="d20_50.gif")
 d20_img = Label(root, image=d20_image)
 d20_img.grid(row=10, column=2)
 
-  # d100
+ # Entry for a d100 - die with 100 sides, also known as a percentile roll
 d100_label = tk.Label(root, text="Quantity of d100's: ")
 d100_label.grid(row=11, column=0)
 d100_entry = Spinbox(root, from_=0, to=50, width=3, validate="key", 
                         validatecommand=(root.register(validate_input), "%P"))
 d100_entry.grid(row=11, column=1)
 
-  # d custom
+ # Entry for a custom roll - user chooses the number of sides (the random number will be
+ # between 1 and what the end user enters) and quantity
 custom_header_label = tk.Label(root, text="Custom", font='bold')
 custom_header_label.grid(row=12, column=0, columnspan=3)
 custom_sides_label = tk.Label(root, text="Number of sides: ")  
@@ -145,34 +147,34 @@ def roll_dice():
             10: int(d10_entry.get()),
             12: int(d12_entry.get()),
             20: int(d20_entry.get()),
-            100: int(d100_entry.get())
+            100: int(d100_entry.get()),
         }
 
-        # Custom dice
+        # Declare sides and quantity for custom roll
         custom_sides = custom_sides_entry.get()
-        if custom_sides and int(custom_sides) > 0:
-            custom_sides = int(custom_sides)
-            custom_quantity = int(custom_quantity_entry.get())
-            if custom_quantity > 0:
-                dice_quantities[custom_sides] = custom_quantity
-            else:
-                messagebox.showerror("Error", "Please enter a valid integer.")
-                return
+        if custom_sides and int(custom_sides) > 0: # Verify custom sides has a value and is a positive integer
+            custom_sides = int(custom_sides)       # Declare value for custom die size (number of sides)
+            custom_quantity = int(custom_quantity_entry.get()) # Get value for custom die quantity
+            if custom_quantity > 0:                # Verify custom die quantity as a positive integer
+                dice_quantities[custom_sides] = custom_quantity #Declare custom die quantity
+            else:                                  # If custom die quantity is not a positive integer
+                messagebox.showerror("Error", "Please enter a valid integer.") 
+                return                             # Stop this function from running
         else:
-            pass
+            pass                                   # If custom sides is blank, skip rolling it
             
         # Roll dice and display results
         result = ""
-        for sides, quantity in dice_quantities.items():
-            if quantity > 0:
-                rolls = [random.randint(1, sides) for _ in range(quantity)]
-                result += f"{quantity} d{sides}'s: {rolls}\n"
+        for sides, quantity in dice_quantities.items():   # For loop to find the random number for each roll
+            if quantity > 0:                              # Verify there is a quantity greater than 0
+                rolls = [random.randint(1, sides) for _ in range(quantity)]  # Create the random number for roll
+                result += f"{quantity} d{sides}'s: {rolls}\n"                # Create string for output
 
         result_text.insert(tk.END, result)
     except ValueError:
-        messagebox.showerror("Error", "Please enter a valid integer for the number of sides.")
+        messagebox.showerror("Error", "Please enter a valid integer.")
         return
-    open_results_window()
+    open_results_window()                                # After all rolls are done with no invalid entries, open the results window
 
 
 # Function to open a new window with the results
